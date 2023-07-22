8d35e645eb3dc03a468d9980f73ea9ab6d774cb1
chaudhuree <chaudhuree00@gmail.com>
2023-07-23 04:21:11 +0600

    using data from obj and array

--- basic_101/src/App.vue
+++ basic_101/src/App.vue
@@ -1,15 +1,21 @@
 <script setup>
-const message="Welcome. Get ready to master Vue.js 3!"
-const date="30th June, 2023"
+const message = "Welcome. Get ready to master Vue.js 3!";
+const date = "30th June, 2023";
+const getDate = () => {
+  const date = new Date();
+  return date.toDateString();
+};
 </script>
 
 <template>
-<div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-        <h1 class="text-4xl">{{ message }}</h1>
-        <h2 class="mt-10 text-xl text-gray-700">Today is <span>{{ date }}</span></h2>
-    </div>
+  <div
+    class="container mx-auto flex items-center justify-center min-h-screen flex-col"
+  >
+    <h1 class="text-4xl">{{ message }}</h1>
+    <h2 class="mt-10 text-xl text-gray-700">
+      Today is <span>{{ getDate() }}</span>
+    </h2>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
